aea4b15930ff099d3e57ff2ade0660db86af09b4
ehmmadaejazz <ehmmadaejazz33@gmail.com>
2025-02-18 16:43:21 +0530

    skeleton

--- client/src/skeleton/CategoryHomeSkeleton.jsx
+++ client/src/skeleton/CategoryHomeSkeleton.jsx
@@ -5,11 +5,11 @@ const CategoryHomeSkeleton = () => {
      {
           new Array(10).fill(null).map((_,index)=>(
             <div key={index} className=" min-h-36 rounded p-4 bg-white grid gap-2 shadow animate-pulse">
-            <div className="min-h-20 bg-blue-100 rounded "></div>
-            <div className="bg-blue-100 h-8 rounded"></div>
+            <div className="min-h-20 bg-blue-100 rounded ">{""}</div>
+            <div className="bg-blue-100 h-8 rounded">{""}</div>
             <div className="grid gap-4 grid-cols-2">
-              <div className="bg-blue-100 h-8 rounded"></div>
-              <div className="bg-blue-100 h-8 rounded"></div>
+              <div className="bg-blue-100 h-8 rounded">{""}</div>
+              <div className="bg-blue-100 h-8 rounded">{""}</div>
             </div>
             </div>
           ))
